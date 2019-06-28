a9887784b7166494c326094b411011c2cfb243f3
simba <simba3310@gmail.com>
2019-06-28 12:40:19 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var pageBanner = document.getElementById("page-banner");
+var bookList = document.getElementById("book-list");
+var titles = document.getElementsByClassName("title");
+var lists = document.getElementsByTagName("li");
+var bookNames = document.getElementsByClassName("name");
+var wrapper = document.querySelector('#wrapper');
+var secondBook = document.querySelector('#book-list li:nth-child(2) .name');
+
