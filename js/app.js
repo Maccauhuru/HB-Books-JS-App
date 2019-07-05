c31cd7a339b3d1bff2dced19bf678282e23abbed
simba <simba3310@gmail.com>
2019-07-04 19:19:42 -0500

    Node and DOM traversal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var pageBanner = document.getElementById("page-banner");
-var bookList = document.getElementById("book-list");
-var titles = document.getElementsByClassName("title");
+var mainTitle = document.querySelector(".title");
 var lists = document.getElementsByTagName("li");
 var bookNames = document.getElementsByClassName("name");
 var wrapper = document.querySelector('#wrapper');
@@ -8,7 +7,36 @@ var secondBook = document.querySelector('#book-list li:nth-child(2) .name');
 var allSpanTags = document.querySelectorAll('#book-list li .name')
 var bookList = document.querySelector('#book-list');
 
+console.log("-------Chapters 1 - 5 : Query Selectors-------");
+console.log("-------Chapters 1 - 5 : Node Types & Clones-------");
 //Array.from(allSpanTags).forEach(book => console.log(book.textContent += "Hardy Boys Book#"));
 bookList.innerHTML = '<div><h2>Original Hardy Boys Book Series</h2></div>';
 bookList.innerHTML += '<p>Hardy Boys Book #1</p>';
+mainTitle.textContent = "Hardy Boys Books";
 console.log(`The banner node type is : ${pageBanner.nodeType}`);
+console.log(`The banner node name is : ${pageBanner.nodeName}`);
+console.log(`The banner node has child nodes? : ${pageBanner.hasChildNodes()}`);
+
+const clonedBookList = bookList.cloneNode(true);
+console.log(clonedBookList);
+
+console.log("-------Chapter 6 : Nodes-------");
+//Get the parent node of book list
+console.log('The parent node of bookList is :', bookList.parentNode);
+console.log('The parent element of bookList is :', bookList.parentElement);
+console.log('The parent element theparent of bookList is :', bookList.parentElement.parentNode);
+console.log("-------Chapter 7 : Children Nodes-------");
+//Get all the child Elements
+console.log(bookList.childNodes); //Returns a NodeList
+console.log(bookList.children); //Returns an HTMLCollection
+console.log(bookList.children); //Returns an HTMLCollection
+
+console.log("-------Chapter 8 : Sibling Nodes-------");
+console.log('next sibling', bookList.nextSibling); //Returns next Sibling
+console.log('next element sibling', bookList.nextElementSibling); //Returns the next Sibling element
+console.log('previous sibling', bookList.previousSibling);
+console.log('previous Element sibling', bookList.previousElementSibling); //Returns the next Sibling element
+
+bookList.previousElementSibling.querySelector('p').innerHTML += "<br />Cradle To The Grave";
+
+console.log("-------Chapter 9 : Events-------");
---
+++ js/app2.js
@@ -0,0 +1,7 @@
+var bookList = document.querySelector('#book-list');
+
+
+
+//Get the parent node of book list
+console.log(bookList.parentElement);
+console.log();
