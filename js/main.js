5aa5ad4152e3ac6265dc92fed965813b17c97799
simba <simba3310@gmail.com>
2019-07-23 18:35:34 -0500

    Form selectors and created new elements

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 const bookList = document.querySelector('#book-list ul');
-
-
-
-
-
+const addBookForm = document.forms['add-book'];
 
 //Delete books from list
 bookList.addEventListener('click', (e) => {
@@ -12,3 +8,23 @@ bookList.addEventListener('click', (e) => {
         bookList.removeChild(li);
     }
 });
+
+//Add books to list
+addBookForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inputValue = addBookForm.querySelector('input[type="text"]').value;
+
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content to elements
+    li.textContent = inputValue;
+    deleteBtn.textContent = 'delete';
+
+    //append the elements to DOM
+    li.appendChild(bookName);
+    li.appendChild(bookName);
+    bookList.appendChild(li)
+})
